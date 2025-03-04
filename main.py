8f4bd6d0a47e29a30f53876d0f1d8a37afb2d7e7
YESSPER13 <YESSPER13@GMAIL.COM>
2025-03-04 18:49:44 -0500

    feat se crea main con opciones

---
+++ main.py
@@ -0,0 +1,11 @@
+nombreVendedor = None
+productos = []
+producto = {}
+
+opcion = 100
+
+print("Mercado")
+print("1. Crear lista mercado")
+print("2. Preciona 5 para salir")
+while opcion != 5:
+    opcion = int(input("Elija una opción: "))
